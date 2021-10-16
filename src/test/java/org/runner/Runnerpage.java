f15d736428ef52b24f4918d2a2f3ca17adf67ba7
Subash2909 <rajasubash2909@gmail.com>
2021-10-16 17:16:28 +0530

    Change in branch B

--- src/test/java/org/runner/Runnerpage.java
+++ src/test/java/org/runner/Runnerpage.java
@@ -3,10 +3,11 @@ package org.runner;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src\\test\\resources",glue= {"org.adactstep"})
+@CucumberOptions(features="src\\test\\resources",glue= {"org.adactstep"}, dryRun = false, monochrome = true,snippets = SnippetType.CAMELCASE, strict = false)
 public class Runnerpage {
 
 }
